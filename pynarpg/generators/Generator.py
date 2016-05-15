654cd187ab23c4b5e4b53ab555ebb7d33d8aad6d
Evan Kirsch <etkirsch@gmail.com>
2016-05-15 10:44:41 -0400

    Added template and generate(num) to Generator

--- pynarpg/generators/Generator.py
+++ pynarpg/generators/Generator.py
@@ -1,4 +1,5 @@
 from pynarpg.model.RpgEntity import RpgEntity
+import random
 
 class Generator(RpgEntity):
     def __init__(self, type_to_generate):
@@ -7,11 +8,24 @@ class Generator(RpgEntity):
         self.type_to_generate = split_results[-1]
         self.module = __import__(self.module_name)
 
-    def generate(self, yield_number):
+    def generate(self, yield_number, generation_parameters):
         '''Generates a certain number of elements'''
-        self.create_one()
+        return [self.create_one(generation_parameters) for x in range(0,yield_number)]
 
-    def create_one(self):
+    def create_template(self):
+        '''Create a blank template'''
         if not hasattr(self.module, self.type_to_generate):
             return
         return getattr(self.module, self.type_to_generate)()
+
+    def create_one(self, generation_parameters):
+        '''
+        Create an object using generation_parameters to specify some sort of
+        stochasticity in generation. For instance, you may pass in
+        {'value': range(0,10)} to allow the generator to specify that the
+        resultant object.value is any number in that specified range.
+        '''
+        shell = self.create_template()
+        for param in [x for x in generation_parameters if hasattr(shell, x)]:
+            setattr(shell, param, random.choice(generation_parameters[param]))
+        return shell
--- tests/generators/GeneratorTests.py
+++ tests/generators/GeneratorTests.py
@@ -7,8 +7,28 @@ class GeneratorTests(unittest.TestCase):
         self.assertEqual('pynarpg.inventory', g.module_name)
         self.assertEqual('Armor', g.type_to_generate)
 
-    def test_create_one(self):
+    def test_create_template(self):
         g = Generator('pynarpg.inventory.Armor')
-        result = g.create_one()
+        result = g.create_template()
 
         self.assertIsNotNone(result)
+
+    def test_create_one(self):
+        g = Generator('pynarpg.inventory.Armor')
+        gen_params = { \
+            'max_dex_mod': range(3,4), \
+            'armor_class_modifier': range(3,4)}
+        result = g.create_one(gen_params)
+
+        self.assertIsNotNone(result)
+        self.assertTrue(result.armor_class_modifier > 2)
+        self.assertTrue(result.max_dex_mod > 2)
+
+    def test_generate(self):
+        g = Generator('pynarpg.inventory.Armor')
+        gen_params = { \
+            'max_dex_mod': range(3,4), \
+            'armor_class_modifier': range(3,4)}
+
+        result = g.generate(10, gen_params)
+        self.assertEqual(10, len(result))
