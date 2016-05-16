7da86ad377953b7a9e857a3fa3e6eb4af4b6f6ba
Evan Kirsch <etkirsch@gmail.com>
2016-05-16 13:08:37 -0400

    FURTHER improved rolling syntax

--- pynarpg/model/RpgEntity.py
+++ pynarpg/model/RpgEntity.py
@@ -6,12 +6,11 @@ class RpgEntity:
     def regex(self, to_evaluate):
         '''Regex on the damage string'''
         captured = re.search(RpgEntity.nDxy_expression, to_evaluate)
-        return [int(x) for x in captured.groups() if x is not None]
+        return [int(x) if x is not None else 0 for x in captured.groups()]
 
     def roll(self, to_evaluate):
         '''Roll on a string such as '1d20' or '6d6+6' '''
-        regex = self.regex(to_evaluate)
-        num, die, mod = [regex[i] if i < len(regex) else 0 for i in range(0,3)]
+        num, die, mod = self.regex(to_evaluate)
         return sum([self.individual_roll(die) + mod for x in range(0,num)])
 
     def individual_roll(self, die):
--- tests/model/RpgEntityTests.py
+++ tests/model/RpgEntityTests.py
@@ -4,9 +4,10 @@ import unittest
 class RpgEntityTests(unittest.TestCase):
     def test_regex_no_mod(self):
         w = RpgEntity()
-        num, die = w.regex('1d6')
+        num, die, mod = w.regex('1d6')
         self.assertEqual(num, 1)
         self.assertEqual(die, 6)
+        self.assertEqual(mod, 0)
 
     def test_regex_with_mod(self):
         w = RpgEntity()
