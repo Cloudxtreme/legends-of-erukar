0cd78a6a0dc382ed3252164d6f56c5787fba665b
Evan Kirsch <etkirsch@gmail.com>
2016-05-17 14:05:49 -0400

    added travis, Open Command

--- pynarpg/commands/Open.py
+++ pynarpg/commands/Open.py
@@ -1,4 +1,49 @@
-from pynarpg.model.Command import Command
+from pynarpg.model.DirectionalCommand import DirectionalCommand
+from pynarpg.environment.Door import Door
 
-class Open(Command):
-    pass
+class Open(DirectionalCommand):
+    nesw_success = 'You have successfully opened the door'
+    nesw_locked = 'You try to open the door, but it is locked'
+    nesw_already_open = 'The door is already open'
+    nesw_no_door = 'There is no door in this direction to open'
+    nesw_wall = 'You cannot open a wall'
+
+    # I'm not terribly happy with this, as it forces usage of only Doors.
+    def execute(self, room, payload):
+        player = self.find_player()
+        direction = self.determine_direction(payload.lower())
+
+        # If the payload was NESW, treat this as a door
+        if direction is not None:
+            return self.handle_doors(room, direction)
+
+        # Otherwise we need to find in the room
+
+    def handle_doors(self, room, direction):
+        '''
+        Treat this command as an open doors command, since the user typed in
+        a direction
+        '''
+        in_direction = room.get_in_direction(direction)
+
+        # No connections have been made in this direction
+        if in_direction is None:
+            return Open.nesw_wall
+
+        # determine if the door prevents movement
+        door = in_direction["door"]
+        if door is None:
+            # There is no door to open
+            return Open.nesw_no_door
+
+        # Door is locked
+        if door.status is Door.Locked:
+            return Open.nesw_locked
+
+        # Door is already open
+        if door.status is Door.Open:
+            return Open.nesw_already_open
+
+        # Actually open the door
+        door.status = Door.Open
+        return Open.nesw_success
--- pynarpg/environment/Door.py
+++ pynarpg/environment/Door.py
@@ -11,14 +11,3 @@ class Door(EnvironmentPiece, Interactible):
 
     def on_inspect(self, *_):
         return "This is a door"
-
-    def on_open(self, *_):
-        '''Used when the system receives an 'Open' command'''
-        if self.status is Door.Open:
-            return 'Door is already open'
-
-        if self.status is Door.Locked:
-            return 'Door cannot be opened because it is locked'
-
-        self.status = Door.Open
-        return 'Door opened'
