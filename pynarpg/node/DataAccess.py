1c8cd8dd521ddfb3bf594d6535bc5d136e400737
Evan Kirsch <etkirsch@users.noreply.github.com>
2016-05-24 22:27:23 -0400

    Made DataAccess.find_player more straightforward

--- pynarpg/node/DataAccess.py
+++ pynarpg/node/DataAccess.py
@@ -3,6 +3,4 @@ class DataAccess:
         self.players = []
 
     def find_player(self, uid):
-        results = [p for p in self.players if p.uid == uid]
-        if len(results) > 0:
-            return results[0]
+        return next((p for p in self.players if p.uid == uid), None)
