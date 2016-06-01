f4ee000e5c54e905597ca0cc4deb9c286d5401f8
Evan Kirsch <etkirsch@gmail.com>
2016-06-01 10:11:48 -0400

    Added biases to dungeon generation

--- erukar/game/DungeonGenerator.py
+++ erukar/game/DungeonGenerator.py
@@ -6,18 +6,23 @@ import math, random
 
 class DungeonGenerator(FactoryBase):
     minimum_rooms = 3
+
     def __init__(self):
+        self.hallway_bias = 0.6
+        self.branching_probabilities = [0.0, 0.70, 0.90]
         # the following will belong on a Dungeon object that will be passed around
         self.dungeon_map = {}
         self.rooms = []
 
     def generate(self):
-        num_rooms = math.floor(np.random.gamma(7.5, 1.0)) + DungeonGenerator.minimum_rooms
+        num_rooms = math.floor(np.random.gamma(15, 1.0)) + DungeonGenerator.minimum_rooms
+        print('There are {0} rooms in this dungeon'.format(num_rooms))
         return self.create_rooms(num_rooms)
 
     def create_rooms(self, num_rooms):
         '''Create X number of rooms'''
         self.rooms = [Room() for x in range(num_rooms)]
+        self.dungeon_map[(0,0)] = self.rooms[0]
         self.connect_rooms()
         self.generate_descriptions()
         self.fill_walls()
@@ -46,16 +51,13 @@ class DungeonGenerator(FactoryBase):
 
     def connect_rooms(self):
         '''Connect a list of rooms to each other'''
-        self.dungeon_map[(0,0)] = self.rooms[0]
-
-        for origin_room, index in zip(self.rooms, range(len(self.rooms))):
-            self.generate_connections(origin_room, index)
-
-    def connected(self):
-        return [r for r in self.rooms if len(self.possible_directions(r)) < 4]
+        for origin, index in zip(self.rooms, range(len(self.rooms))):
+            num_connections = self.number_of_connections()
+            for connection_num in range(num_connections):
+                self.connect_randomly(origin)
 
     def unconnected(self):
-        return [r for r in self.rooms if len(self.possible_directions(r)) == 4]
+        return [r for r in self.rooms[1:] if len(self.possible_directions(r)) == 4]
 
     def generate_descriptions(self):
         ''' Add descriptions '''
@@ -68,42 +70,59 @@ class DungeonGenerator(FactoryBase):
             for direction in self.possible_directions(room):
                 room.connections[direction] = { 'door': Wall() }
 
-    def generate_connections(self, origin, index):
-        num_connections = max(0, len(self.rooms) - index - self.number_of_connections())
-        for connection_num in range(num_connections):
-            self.connect_randomly(origin)
-
     def connect_randomly(self, origin):
         '''Connect a room (origin) to a destination room in some random direction'''
-        directions = self.possible_directions(origin)
-        if not any(directions): return
+        if not any(self.possible_directions(origin)): return
 
-        # Use the origin room and look in each direction
-        # if there's a room there, connect to it.
-        # If not, grab a room with empty coordinates that isn't origin
-
-        direction = random.choice(directions)
+        direction = self.random_direction(origin)
         new_coords = self.to_coordinate(origin.coordinates, direction)
         if new_coords in self.dungeon_map:
             destination = self.dungeon_map[new_coords]
         else:
-            if len(self.unconnected()) == 0:
-                return
-            destination = self.unconnected()[0]
+            uncon = self.unconnected()
+            if len(uncon) == 0: return
+            destination = uncon[0]
 
         origin.coestablish_connection(direction, destination)
-        destination.coordinates = self.to_coordinate(origin.coordinates, direction)
-        self.dungeon_map[(destination.coordinates)] = destination
+        self.link_coordinates_to_room(new_coords, destination)
 
+    def link_coordinates_to_room(self, coordinates, destination):
+        '''
+        Links the room's coordinates and the dungeon map's coordinates, removing any
+        duplications
+        '''
+        destination.coordinates = coordinates
+        pre_existing = [r for r in self.dungeon_map if self.dungeon_map[r] is destination]
+        for room in pre_existing:
+            self.dungeon_map.pop(room, None)
+        self.dungeon_map[(destination.coordinates)] = destination
 
     def possible_directions(self, room):
         '''Get all directions for a room that are not already specified'''
         return [d for d in room.connections if room.connections[d] is None]
 
-    def number_of_connections(self):
-        # y = 1 + heaviside(x-0.6) + heaviside(x-0.9) from x=[0, 1.0]
+    def random_direction(self, room):
+        '''Find a random direction implementing biases'''
+        # Build out the random_set using biases and an existing room connection
+        basis_direction = next((x for x in room.connections if room.connections[x] is not None), Direction.North)
+        dirs = (np.arange(3) + basis_direction.value + 1) % 4
+        side_bias = (1.0-self.hallway_bias) / 2
+        random_set = [0, side_bias, side_bias + self.hallway_bias]
+
+        # evaluate a random number vs. the random_set
         rand = random.random()
-        return 1 + sum([1 for pct in [0.66, 0.92] if rand > pct])
+        directional_matches = [dirs[i] for i in range(len(dirs)) if rand > random_set[i]]
+        return Direction(directional_matches[-1])
+
+    def number_of_connections(self):
+        '''
+        Get a random number of connections, where 70% of the time there will be
+        a single connection, 20% of the time there will be two, and 10% of the
+        time there will be three. Probabilities are determined by the constant
+        branching_probabilities
+        '''
+        rand = random.random()
+        return sum([1 for pct in self.branching_probabilities if rand > pct])
 
     def to_coordinate(self, origin_coord, direction):
         '''Hmm, how do I clean this up?'''
